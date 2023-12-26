30bc59b1df6b139dc97a21844f219f33b3ce48d1
Kostis Bourlas <kostisbourlas@protonmail.com>
2023-12-26 13:28:47 +0200

    Parse return statements

--- parser/parser.go
+++ parser/parser.go
@@ -45,6 +45,8 @@ func (p *Parser) parseStatement() ast.Statement {
     switch p.curToken.Type {
     case token.LET:
         return p.parseLetStatement()
+    case token.RETURN:
+        return p.parseReturnStatement()
     default:
         return nil
     }
@@ -62,6 +64,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
     if !p.expectPeek(token.ASSIGN) {
         return nil
     }
+
+    // TODO: We are skipping the expressions until we
+    // encounter a semicolon
+    for !p.curTokenIs(token.SEMICOLON) {
+        p.NextToken()
+    }
+
+    return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+    stmt := &ast.ReturnStatement{Token: p.curToken}
+    p.NextToken()
 
     // TODO: We are skipping the expressions until we
     // encounter a semicolon
