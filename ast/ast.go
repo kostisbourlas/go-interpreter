b59857b04ffe6848b48d7befa4f4f056c76517eb
Kostis Bourlas <kostisbourlas@protonmail.com>
2024-01-03 21:58:18 +0200

    Add ExpressionStatement and implement String() method

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,14 @@
 package ast
 
 import (
-    "github.com/kostisbourlas/go-interpreter/token"
+	"bytes"
+
+	"github.com/kostisbourlas/go-interpreter/token"
 )
 
 type Node interface {
     TokenLiteral() string
+    String() string
 } 
 
 type Statement interface {
@@ -30,6 +33,20 @@ func (ls *LetStatement) TokenLiteral() string {
     return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+    var buf bytes.Buffer
+
+    buf.WriteString(ls.TokenLiteral() + " ")
+    buf.WriteString(ls.Name.String())
+    buf.WriteString(" = ")
+    if ls.Value != nil {
+        buf.WriteString(ls.Value.String())
+    }
+    buf.WriteString(";")
+
+    return buf.String()
+}
+
 type ReturnStatement struct {
     Token token.Token // the 'return' token
     ReturnValue Expression
@@ -39,6 +56,36 @@ func (rs *ReturnStatement) statementNode() {}
 
 func (rs *ReturnStatement) TokenLiteral() string {
     return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+    var buf bytes.Buffer
+
+    buf.WriteString(rs.TokenLiteral() + " ")
+    if rs.ReturnValue != nil {
+        buf.WriteString(rs.ReturnValue.String())
+    }
+    buf.WriteString(";")
+
+    return buf.String()
+}
+
+type ExpressionStatement struct {
+    Token token.Token
+    Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+    return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+    if es.Expression != nil {
+        return es.Expression.String()
+    }
+    return ""
 }
 
 type Identifier struct {
@@ -52,6 +99,10 @@ func (i *Identifier) TokenLiteral() string {
     return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+   return i.Value 
+}
+
 type Program struct {
     Statements []Statement
 }
@@ -61,4 +112,14 @@ func (p *Program) TokenLiteral() string {
         return ""
     }
     return p.Statements[0].TokenLiteral()
+}
+
+func (p *Program) String() string {
+    var buf bytes.Buffer
+
+    for _, s := range p.Statements {
+        buf.WriteString(s.String())
+    }
+
+    return buf.String()
 }
